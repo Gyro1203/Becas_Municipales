8b897bb1efeabd77e2983e80b793e8798dc5eb2c
Lukakao <138331238+Lukakao@users.noreply.github.com>
2023-10-13 01:45:49 -0300

    CRUD resultados postulaciones

---
+++ backend/src/routes/resultados_postulaciones.route.js
@@ -0,0 +1,11 @@
+"use strict";
+const express = require("express");
+const router = express.Router();
+const resultadosController = require("../controllers/resultado_postulacion_controller.js");
+
+router.get("/resultados", formController.getForms);
+router.post("/submitresultado", formController.createFrom);
+router.del("/delresultado", formController.getForms);
+router.patch("/editresultado", formController.createFrom);
+
+module.exports = router;
