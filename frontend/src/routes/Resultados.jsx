55ad6c636309a0caf8b92e67a3740f293a75271d
Lukakao <138331238+Lukakao@users.noreply.github.com>
2023-12-18 11:40:48 -0300

    apenas funciona

--- frontend/src/routes/Resultados.jsx
+++ frontend/src/routes/Resultados.jsx
@@ -3,8 +3,7 @@ import instance from '../services/root.service';
 import NavBar from '../components/NavBar';
 import { AuthProvider, useAuth } from '../context/AuthContext';
 import axios from 'axios';
-import {ListItemButton, Box,Divider,Stack,IconButton,DialogTitle,Dialog, DialogContent, Grid, List,ListItem, ListItemText} from '@mui/material'
-import CancelIcon from '@mui/material/Cancel';
+import {ListItemButton, Box,Button,Divider,Stack,IconButton,DialogTitle,Dialog, DialogContent, Grid, List,ListItem, ListItemText} from '@mui/material'
 
 useAuth;
 
@@ -14,32 +13,55 @@ useAuth;
 
 const MenuAceptarRechazar = (data) => {
     const [isAbierto, setAbierto] = useState(true);
-    /*
-    useEffect(() => {
-        instance.get(`/resultados/${id}`)
+    
+
+    const handleAceptar = () =>{
+        const body ={
+            codigo_postulacion:data.data._id,
+            isAceptado: true,
+        }
+        instance.post(`/resultados/`,body)
         .then((response) => {
             const res = response;
-            setPostulaciones(res.data.data);
-            console.log(res.data.data);
+            toggleAbierto
+            console.log(res);
+
         })
         .catch((error) => {
             console.log(error);
         })
-    },[])
-    */
-
-    const onClick = () =>{
-        setAbierto(false);
     }
 
-    console.log("asdasdsa", data);
+
+    const handleRechazar = () =>{
+
+        const body ={
+            codigo_postulacion:data.data._id,
+            isAceptado:false,
+            razon: "   asdasd    asdasd     aa"
+        }
+        instance.post(`/resultados/`,body)
+        .then((response) => {
+            const res = response;
+            toggleAbierto
+            console.log(res);
+
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    const toggleAbierto = () =>{
+        setAbierto(!isAbierto);
+    }
     return (
         <>
         <Dialog open={isAbierto}>
             <DialogTitle>
                 Revisar Postulacion
-                <IconButton onClick={onClick} color="inherit">
-                    <CancelIcon />
+                <IconButton onClick={toggleAbierto} color="inherit">
+                <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>
                 </IconButton>
                 </DialogTitle>
                 <DialogContent>
@@ -53,13 +75,22 @@ const MenuAceptarRechazar = (data) => {
                         <Divider />
                         <p>Tipo de beca: {data.data.typeBeca}</p>
                         <p>Fecha de postulacion: {data.data.createdAt.split("T")[0]}</p>
+                        <Divider/>
+                        <Box display="flex" justifyContent="space-between">
+
+                            <Button sx={{ backgroundColor: 'red', color: 'white' }} onClick={handleRechazar}>
+                                Rechazar
+                            </Button>
+
+                            <Button sx={{ backgroundColor: 'green', color: 'white' }} onClick={handleAceptar}>
+                                Aceptar
+                            </Button>
+                        </Box>
+
                     </div>
                 </DialogContent>
         </Dialog>
-        
         </>
-
-
     )
 
 }
@@ -76,7 +107,6 @@ const Resultados = () => {
         .then((response) => {
             const res = response;
             setPostulaciones(res.data.data);
-            console.log(res.data.data);
         })
         .catch((error) => {
             console.log(error);
@@ -84,7 +114,7 @@ const Resultados = () => {
     },[])
 
     const abrirMenu = (postulacion) => {
-        setAbrir(true)
+        setAbrir(!isAbrir)
         setSelected(postulacion)
     }
     
@@ -109,8 +139,12 @@ const Resultados = () => {
         </ListItemButton>
     ))}
     </List>
-    {isAbrir && 
-        <MenuAceptarRechazar data={selectedPostulacion}/>}
+    {isAbrir ? (
+        <MenuAceptarRechazar data={selectedPostulacion}/>
+    ) : (
+        <></>
+    )
+        }
     </>
     );
 
