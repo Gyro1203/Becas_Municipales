de11939e04bea5c396672fc116024d58fe969bbd
Lukakao <138331238+Lukakao@users.noreply.github.com>
2023-12-18 09:06:43 -0300

    listado y menu popup

--- frontend/src/routes/Resultados.jsx
+++ frontend/src/routes/Resultados.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react';
+import instance from '../services/root.service';
 import NavBar from '../components/NavBar';
 import { AuthProvider, useAuth } from '../context/AuthContext';
+import axios from 'axios';
+import ListItemButton from '@mui/material/ListItemButton'
 
-function Resultados(){
+useAuth;
+
+const Resultados = ()  => {
+    const [postulaciones, setPostulaciones] = useState([]);
+    const [selected, setSelected] = useState(false);
+    const [selectedId, setSelectedId] = useState(0);
+
+    useEffect(() => {
+        instance.get('/resultados/postulaciones')
+        .then((response) => {
+            const res = response.data;
+            setPostulaciones(res);
+            console.log("res ", res);
+        })
+        .catch((error) => {
+            console.log('Error',error);
+        })
+    })
 
     return(
     <>
     <NavBar/>
     <h1>Resultados</h1>
+    <ListItemButton></ListItemButton>
     </>
     );
 
